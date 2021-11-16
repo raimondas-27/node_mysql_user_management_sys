04377a5153455be7cf0253c474558a05d78ad31d
raimondas-27 <raimondasvilnius@yahoo.com>
2021-11-16 23:24:00 +0200

    added some rules for creating a user in phpmyadmin sql queries

--- user-schema.sql
+++ user-schema.sql
@@ -1,1 +1,13 @@
+//need it if there is an error with connection
+
+CREATE USER 'username'@'localhost' IDENTIFIED BY 'password';
+
+GRANT ALL PRIVILEGES ON *.* TO 'username'@'localhost' WITH GRANT OPTION;
+
+CREATE USER 'username'@'%' IDENTIFIED BY 'password';
+
+GRANT ALL PRIVILEGES ON *.* TO 'username'@'%' WITH GRANT OPTION;
+
+FLUSH PRIVILEGES;
+
 CREATE TABLE `usermanagement_node`.`user` ( `id` INT NOT NULL AUTO_INCREMENT , `first_name` VARCHAR(45) NOT NULL , `last_name` VARCHAR(45) NOT NULL , `email` VARCHAR(45) NOT NULL , `phone` VARCHAR(45) NOT NULL , `comments` TEXT NOT NULL , `status` VARCHAR NOT NULL DEFAULT 'active' , PRIMARY KEY (`id`)) ENGINE = InnoDB;
