cc23fb96a60fc25eb1a3707cb81d13448630734d
raimondas-27 <raimondasvilnius@yahoo.com>
2021-12-09 00:44:09 +0200

    added alert for adding user

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require("path");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // parsing middleware
 // parse application/x-www-form-urlencoded
